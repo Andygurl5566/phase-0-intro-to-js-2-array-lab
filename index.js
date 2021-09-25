2c05ee4f8fa7a15d99541040ede46fb0021e52f9
Andrea Freeman <afreeman5566@gmail.com>
2021-09-25 18:06:52 -0500

    commit tree 033acb72084123d6d3ad5af5874ffd465ca0d51d

--- index.js
+++ index.js
@@ -1,44 +1,82 @@
 // Write your solution here!
+// let cats = ["Milo", "Otis", "Garfield"]
+
+// function destructivelyAppendCat(name){
+//     cats.push("Ralph")
+// }
+
+// function destructivelyPrependCat(name){
+//     cats.unshift("Bob")
+// }
+
+// function destructivelyRemoveLastCat(){
+//     cats.pop()
+// }
+
+// function destructivelyRemoveFirstCat(){
+//     cats.shift()
+// }
+
+
+
+
+// function appendCat(name){
+//     let newcats = [...cats,"Broom"]
+//     return newcats
+// }
+
+
+// function prependCat(name){
+//     let newcats = ["Arnold",...cats]
+//     return newcats
+// }
+
+// function removeLastCat(){
+//    let newcats = [...cats.slice(0,2)]
+//    return newcats
+
+// }
+
+// function removeFirstCat(){
+//     let newcats = [...cats.slice(1,3)]
+//     return newcats
+// }
 let cats = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendCat(name){
-    cats.push("Ralph")
-}
-
 function destructivelyPrependCat(name){
-    cats.unshift("Bob")
+    return cats.unshift(name)
 }
 
 function destructivelyRemoveLastCat(){
-    cats.pop()
+    return cats.pop()
 }
 
 function destructivelyRemoveFirstCat(){
-    cats.shift()
+    return cats.shift()
+    
 }
 
-
-
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
 
 function appendCat(name){
-    let newcats = [...cats,"Broom"]
+    let newcats = [...cats, name]
     return newcats
 }
-
 
 function prependCat(name){
-    let newcats = ["Arnold",...cats]
+    let newcats = [name,...cats]
     return newcats
 }
 
-function removeLastCat(){
-   let newcats = [...cats.slice(0,2)]
-   return newcats
 
+function removeLastCat(){
+    let newcats = [...cats.slice(0,2)]
+    return newcats
 }
 
-
 function removeFirstCat(){
-    let newcats = [...cats.slice(1,3)]
+    let newcats = [...cats.slice(1)]
     return newcats
 }
